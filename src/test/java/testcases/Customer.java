749cd24da1da1ba6666b3c9226763d58ce8a9eba
Sumit <sumitraut01@gmail.com>
2023-04-10 11:09:05 +0530

    first commit

---
+++ src/test/java/testcases/Customer.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "Customer")
+public class Customer {
+
+	
+	
+	@Test
+	public void Customer_Case1() {		
+		System.out.println("Executing Customer_Case1");
+		
+	}
+	
+	
+	@Test
+	public void Customer_Case2() {		
+		System.out.println("Executing Customer_Case2");
+		
+	}
+}
---
+++ src/test/java/testcases/Transactions.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "Transactions")
+public class Transactions {
+
+	
+	
+	@Test
+	public void Transactions_Case1() {		
+		System.out.println("Executing Transactions_Case1");		
+	}
+	
+	@Test
+	public void Transactions_Case2() {		
+		System.out.println("Executing Transactions_Case2");		
+	}
+}
